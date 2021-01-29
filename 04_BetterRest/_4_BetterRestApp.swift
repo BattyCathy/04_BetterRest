d9ccd6eeff856f3d583dafacb8cacda57815e114
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-01-29 11:40:18 -0800

    Initial Commit

---
+++ 04_BetterRest/_4_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4_BetterRestApp.swift
+//  04_BetterRest
+//
+//  Created by Austin Roach on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct _4_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
